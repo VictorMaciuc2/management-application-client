4bc2566d27b475c959cce0f0387567059b7a2adb
SilviuMelinte <SilviuMelinte@users.noreply.github.com>
2020-11-15 22:57:38 +0200

    departments

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Department } from '../../models/department';
+import {DepartmentsService} from '../../services/departments.service'
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrls: ['./departments.component.css']
+  styleUrls: ['./departments.component.css'],
+  providers: [DepartmentsService]
 })
 export class DepartmentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private departmentsService: DepartmentsService) { }
+  _listFilter = '';
+ 
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredDepartaments = this.listFilter ? this.performFilter(this.listFilter) : this.departaments;}
+    filteredDepartaments: Department[] = [];
+    departaments: Department[] = [];
+  
+    performFilter(filterBy: string): Department[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.departaments.filter((department: Department) =>
+      department.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+    delete(): void{}
+    edit(): void{}
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.departmentsService.getDepartments().subscribe({
+        next: departaments => {
+          this.departaments = departaments;
+          console.log(departaments)
+        this.filteredDepartaments = this.departaments;
+        }
+      //  error: err => this.errorMessage = err
+      });
+    }
   }
 
-}
--- src/app/services/departments.service.ts
+++ src/app/services/departments.service.ts
@@ -1,11 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import { Observable, throwError } from 'rxjs';
+import { Department } from '../models/department';
+import { catchError, tap, map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentsService {
-  private baseUrl = environment.baseApiUrl + '/departments';
+  //private baseUrl = environment.baseApiUrl + '/departments';
+  private baseUrl = 'assets/departments.json';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(this.baseUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data)))
+        //catchError(this.handleError)
+      );
+  }
+
+  getDepartment(id: number): Observable<Department | undefined> {
+    return this.getDepartments()
+      .pipe(
+        map((products: Department[]) => products.find(p => p.id === id))
+      );
+  }
 }
