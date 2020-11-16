b0354cb30a73bc34356dc8062ec178712698691b
Mihalut Filip <mihalutafilip@yahoo.com>
2020-11-16 13:44:23 +0200

    Fix departments frontend

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,12 +9,11 @@ import { User } from '../models/user';
 })
 export class LoginService {
   private baseUrl = environment.baseApiUrl + '/login';
-  private apiUrl = 'http://127.0.0.1:5000/login';
   private httpOptions = { headers: new HttpHeaders({'Content-Type': 'application/json'}) }
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(this.apiUrl, {'email': email, 'password': password}, this.httpOptions)
+    return this.http.post<User>(this.baseUrl, {'email': email, 'password': password}, this.httpOptions);
   }
 }
