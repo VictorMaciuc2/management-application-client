7199b3b8a03be166435e7f6c0db1cec4b0648227
Victor Maciuc <48880098+victormaciuc@users.noreply.github.com>
2020-11-16 11:00:11 +0200

    toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'management-application-client';
-  constructor() { 
+  isLoggedIn: boolean;
+  constructor() {
   }
 }
