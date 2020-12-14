03d261493cd4adab7c38222c4110aadf3944bb3a
Mihalut Filip <mihalutafilip@yahoo.com>
2020-12-14 21:36:43 +0200

    mplement the employee recommendation based on experience

--- src/app/modals/project-modal/project-modal.ts
+++ src/app/modals/project-modal/project-modal.ts
@@ -1,41 +1,25 @@
 import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { Project } from '../../models/project';
 
 @Component({
     styleUrls: ['./project-modal.css'],
     templateUrl: './project-modal.html'
 })
-
 export class ProjectModal implements OnInit {
     public technologies = [];
     public employees = [];
+    public employeesWithRecommandation = [];
     public clients = [];
-    public dropdownSettings: IDropdownSettings;
-    public clientDropdownSettings: IDropdownSettings;
     public project: Project;
     public isTechnologyInputVisible: boolean = false;
     public technologyName: string = '';
-    public contorId: number = 0;
+    public techAndEmployeesGroup;
 
     constructor(public dialogRef: MatDialogRef<ProjectModal>,
         @Inject(MAT_DIALOG_DATA) public data: any) {
-        this.dropdownSettings = <IDropdownSettings>{
-            singleSelection: false,
-            idField: 'id',
-            textField: 'name',
-            selectAllText: 'Select All',
-            unSelectAllText: 'Unselect All',
-            itemsShowLimit: 3,
-            allowSearchFilter: false,
-            showSelectedItemsAtTop: true,
-            enableCheckAll: true
-        };
-
-        this.clientDropdownSettings = {...this.dropdownSettings};
-        this.clientDropdownSettings.singleSelection = true;
     }
 
 
@@ -43,7 +27,45 @@ export class ProjectModal implements OnInit {
         this.project = this.data.project;
         this.technologies = this.data.technologies;
         this.employees = this.data.employees;
+        this.employeesWithRecommandation = this.data.employeesWithRecommandation;
         this.clients = this.data.clients;
+        this.setTechAndEmployeesGroup();
+    }
+
+    setTechAndEmployeesGroup() {
+        this.techAndEmployeesGroup = this.project.technologies.map(tech => {
+            (<any>tech).users = [];
+            return tech;
+        });
+
+        this.techAndEmployeesGroup.push({id: 0, name: 'Others', users: []});
+
+        //delete duplicate and keep maximum experience
+        this.employees.forEach(employee => {
+            var employeeWithMaximumDays;
+            var technology;
+            this.employeesWithRecommandation.forEach(tech => {
+                var user = tech.users.find(user => user.id == employee.id);
+                if(!employeeWithMaximumDays || user.experienceInDays > employeeWithMaximumDays.experienceInDays)
+                {
+                    employeeWithMaximumDays = user;
+                    technology = tech;
+                }
+            })
+            
+            var indexOfTechnology = employeeWithMaximumDays.experienceInDays != 0 ? this.techAndEmployeesGroup.findIndex(tech => tech.id == technology.id): this.techAndEmployeesGroup.length - 1;
+            if(indexOfTechnology < 0)
+                indexOfTechnology = this.techAndEmployeesGroup.length - 1;
+            this.techAndEmployeesGroup[indexOfTechnology].users.push(employeeWithMaximumDays);
+        })
+
+        // delete group with no recommandation
+        this.techAndEmployeesGroup = this.techAndEmployeesGroup.filter(tech => tech.users.length != 0);
+
+        // sort group by experience 
+        this.techAndEmployeesGroup.forEach(tech => {
+            tech.users.sort((a, b) => b.experienceInDays - a.experienceInDays);
+        });
     }
 
     onKeyUp(event) {
@@ -55,8 +77,7 @@ export class ProjectModal implements OnInit {
 
     showAddTechnology() {
         if (this.isTechnologyInputVisible && this.technologyName.length != 0) {
-            // add ids just for UI
-            this.technologies.push({ id: this.contorId--, name: this.technologyName })
+            this.technologies.push({ id: 0, name: this.technologyName })
             this.technologyName = '';
         }
         this.isTechnologyInputVisible = !this.isTechnologyInputVisible;
