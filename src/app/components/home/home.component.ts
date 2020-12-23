b604ea359369fbe29ce780be1731742342a32c17
Mihalut Filip <mihalutafilip@yahoo.com>
2020-12-23 18:52:36 +0200

    Add chart with technology experience

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Technology } from 'src/app/models/technology';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public technologies: Technology[];
+  constructor(private employeeService: EmployeesService) {
+    
+   }
 
   ngOnInit(): void {
+    this.employeeService.getEmployeesByRecommandation().subscribe(result => {
+      this.technologies = result;
+    })
   }
 
 }
