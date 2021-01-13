d935a2e77ec7d2ae4c7a8f378dd96b185db0c50d
FlorinDolha <florin_dolha99@yahoo.com>
2021-01-13 16:40:50 +0200

    move chart from feedback to home

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -10,32 +10,6 @@ import { User } from 'src/app/models/user';
 import { FeedbackService } from 'src/app/services/feedback.service';
 import { StorageService } from 'src/app/services/local-storage.service';
 import { ProjectsService } from 'src/app/services/projects.service';
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
-  ApexStroke,
-  ApexYAxis,
-  ApexTitleSubtitle,
-  ApexLegend
-} from "ng-apexcharts";
-import { series } from "./data";
-import { EmployeesService } from 'src/app/services/employees.service';
-
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  stroke: ApexStroke;
-  dataLabels: ApexDataLabels;
-  yaxis: ApexYAxis;
-  title: ApexTitleSubtitle;
-  labels: string[];
-  legend: ApexLegend;
-  subtitle: ApexTitleSubtitle;
-};
 
 @Component({
   selector: 'app-feedback',
@@ -46,7 +20,6 @@ export class FeedbackComponent implements OnInit {
   user: User;
   sessions: ReportSession[];
   selectedSession: ReportSession;
-  selectedDataRow: any;
   teammates: User[];
   dataSource: MatTableDataSource<User>;
   displayedColumns: string[] = ['name', 'email', 'role', 'seniorityLevel', 'department', 'edit'];
@@ -54,10 +27,6 @@ export class FeedbackComponent implements OnInit {
   seniorityLevel = SeniorityLevel;
   submitValue: User;
   reports: Report[][];
-  dataRows: any[];
-
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
 
   constructor(private storageService: StorageService,
               private feedbackService: FeedbackService,
@@ -68,18 +37,6 @@ export class FeedbackComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.storageService.getLoggedInUser();
     this.getReportSessions();
-    this.getUserGrowth();
-  }
-
-  getUserGrowth(){
-    this.feedbackService.getUserGrowth().subscribe(
-      dataRow => {
-        this.dataRows = dataRow.filter(dataRow => dataRow.user.role === 3);
-
-        this.selectedDataRow = this.dataRows[0];
-        this.createChart();
-      }
-    );
   }
 
   getTeammates(){
@@ -129,57 +86,5 @@ export class FeedbackComponent implements OnInit {
     this.feedbackService.submitReportSession(this.selectedSession, this.reports).subscribe(
       forget => location.reload()
     );
-  }
-
-  createChart(){
-    if (this.selectedDataRow === undefined){
-      return;
-    }
-
-    this.chartOptions = {
-      series: [
-        {
-          name: "Feedback",
-          data: this.selectedDataRow.reports.map(report => report.rating)
-        }
-      ],
-      chart: {
-        type: "area",
-        height: 350,
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: "straight"
-      },
-
-      title: {
-        text: "Skills growth",
-        align: "left"
-      },
-      subtitle: {
-        text: this.selectedDataRow.user.name,
-        align: "left"
-      },
-      labels: this.selectedDataRow.reports.map(report => report.date),
-      xaxis: {
-        type: "datetime",
-        labels: {
-          format: 'dd MMM'
-        }
-      },
-      yaxis: {
-        max: 5,
-        min: 1,
-        opposite: true
-      },
-      legend: {
-        horizontalAlign: "left"
-      }
-    };
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexLegend, ApexStroke, ApexTitleSubtitle, ApexXAxis, ApexYAxis, ChartComponent } from 'ng-apexcharts';
+import { FeedbackService } from 'src/app/services/feedback.service';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  yaxis: ApexYAxis;
+  title: ApexTitleSubtitle;
+  labels: string[];
+  legend: ApexLegend;
+  subtitle: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-home',
@@ -6,10 +21,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  selectedDataRow: any;
+  dataRows: any[];
 
-  constructor() { }
+  constructor(private feedbackService: FeedbackService) { }
 
   ngOnInit(): void {
+    this.getUserGrowth();
   }
 
+  getUserGrowth(){
+    this.feedbackService.getUserGrowth().subscribe(
+      dataRow => {
+        this.dataRows = dataRow.filter(dataRow => dataRow.user.role === 3);
+
+        this.selectedDataRow = this.dataRows[0];
+        this.createChart();
+      }
+    );
+  }
+
+  createChart(){
+    if (this.selectedDataRow === undefined){
+      return;
+    }
+
+    this.chartOptions = {
+      series: [
+        {
+          name: "Feedback",
+          data: this.selectedDataRow.reports.map(report => report.rating)
+        }
+      ],
+      chart: {
+        type: "area",
+        height: 350,
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+
+      title: {
+        text: "Skills growth",
+        align: "left"
+      },
+      subtitle: {
+        text: this.selectedDataRow.user.name,
+        align: "left"
+      },
+      labels: this.selectedDataRow.reports.map(report => report.date),
+      xaxis: {
+        type: "datetime",
+        labels: {
+          format: 'dd MMM'
+        }
+      },
+      yaxis: {
+        max: 5,
+        min: 1,
+        opposite: true
+      },
+      legend: {
+        horizontalAlign: "left"
+      }
+    };
+  }
 }
